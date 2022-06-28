db61d256a26665ae8a4b11a5f947deb2fb48ec55
braty <bora.ataysen@rollicgames.com>
2022-06-28 19:00:03 +0300

    cinemachine added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField][Range(0,20)] private float _moveSpeed;
+    [SerializeField][Range(0,100)] private float _rotationSpeed;
+    [SerializeField][Range(0,50)] private float _zoomAmount;
+    [SerializeField][Range(0,20)] private float _zoomSpeed;
+    [SerializeField] private CinemachineVirtualCamera _cinemachineVirtualCamera;
+
+    private const float MIN_FOLLOW_Y_OFFSET = 2f;
+    private const float MAX_FOLLOW_Y_OFFSET = 12f;
+
+    private CinemachineTransposer _cinemachineTransposer;
+    private Vector3 _targetFollowOffset;
+
+    private void Start()
+    {
+        _cinemachineTransposer = _cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        _targetFollowOffset = _cinemachineTransposer.m_FollowOffset;
+    }
+
+    void Update()
+    {
+        float deltaTime = Time.deltaTime;
+        
+        Vector3 inputMoveDirection = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) inputMoveDirection.z += 1f;
+        if (Input.GetKey(KeyCode.A)) inputMoveDirection.x -= 1f;
+        if (Input.GetKey(KeyCode.S)) inputMoveDirection.z -= 1f;
+        if (Input.GetKey(KeyCode.D)) inputMoveDirection.x += 1f;
+
+        Vector3 moveVector = transform.forward * inputMoveDirection.z + transform.right * inputMoveDirection.x;
+        transform.position += moveVector * deltaTime * _moveSpeed;
+
+        Vector3 rotationVector = Vector3.zero;
+        
+        if (Input.GetKey(KeyCode.Q)) rotationVector.y += 1f;
+        if (Input.GetKey(KeyCode.E)) rotationVector.y -= 1f;
+
+        transform.eulerAngles += rotationVector * deltaTime * _rotationSpeed;
+
+        _targetFollowOffset.y = Mathf.Clamp(_targetFollowOffset.y + Input.mouseScrollDelta.y * _zoomAmount,
+            MIN_FOLLOW_Y_OFFSET,
+            MAX_FOLLOW_Y_OFFSET);
+
+        _cinemachineTransposer.m_FollowOffset = Vector3.Lerp(_cinemachineTransposer.m_FollowOffset, _targetFollowOffset,
+            deltaTime * _zoomSpeed);
+
+
+    }
+}
